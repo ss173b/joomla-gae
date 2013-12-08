935146075cf79984c4b2091558fadaed3e48318d
Gary A. Mort <garyamort@gmail.com>
2013-12-07 23:56:37 -0500

    Config updates with a replacement MYSQLI Joomla DB driver to allow for storage of the CloudSQL connection info[host, port, and socket] along with a check so that when executed inside the GAE environment it uses the socket, and executed outside the environment[dev server] it can use the ip address and port instead.

--- gae/joomla/site.php
+++ gae/joomla/site.php
@@ -6,6 +6,19 @@ libxml_disable_entity_loader(false);
 // Load defined constants for Joomla under GAE
 require_once(__DIR__ . '/defines.php');
 
+
+
+require_once JPATH_BASE . '/includes/framework.php';
+// Pre-empt the normal bootstrapping because we need to make a change
+// Bootstrap the application
+define('JPATH_BASE', JOOMLACMSSITEDIR);
+require_once JPATH_BASE . '/includes/defines.php';
+require_once JPATH_BASE . '/includes/framework.php';
+
+// Add Installation prefix in case we have to over-ride any classes
+// By setting prepend to true, we force our directory to be checked first
+JLoader::registerPrefix('J', GAEJOOMLALIBS, false, true);
+
 //Some file checks are relative to the current working directory, so set to where it would normally be
 chdir (JOOMLACMSSITEDIR);
 
